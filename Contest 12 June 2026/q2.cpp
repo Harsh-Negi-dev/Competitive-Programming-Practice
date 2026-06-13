5a40bc0625894609010209b72ee4a3d5c992b431
Harsh-Negi-dev <harshnegi2311@gmail.com>
2026-06-13 20:53:53 +0530

    Add helper structures template + cp books

---
+++ Contest 12 June 2026/q2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+        for (int i = 0; i <= n - k - 1; i++) {
+            
+        }
+    }
+    return 0;
+}
